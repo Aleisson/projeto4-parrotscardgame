225a298192f85edd968835028a14ce2d1eb4035b
Aleisson <caioaleisson@gmail.com>
2022-06-17 17:11:43 -0300

    function comparador()

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
 // querySelector(".front-face").style.transform = 'rotateY(-180deg)';
 // querySelector(".back-face").style.transform = 'rotateY(0deg)';
