e8efef7575e3021ef01833898b7374c22c6de5c7
Aleisson <caioaleisson@gmail.com>
2022-06-18 18:30:57 -0300

    function flipselect() & flipbegin()

--- script.js
+++ script.js
@@ -29,17 +29,17 @@ function addcard(num) {
 	let cont = num / 2;
 	let arrayCards = [];
 	let divContent = document.querySelector(".content");
-
+	let i = 0;
 	while (cont) {
 		for (let index = 0; index < 2; index++) {
 
 			arrayCards.push(`   <div>
-									<div class="card">
+									<div onclick="flipselect(this)" class="card">
 										<div class="front-face face">
 											<img src="img/front.png" alt="papagaio">
 										</div>
 										<div class="back-face face">
-											<img src="img/${img[cont]}" alt="">
+											<img src="img/${img[i]}" alt="">
 										</div>
 			
 									</div>
@@ -47,14 +47,58 @@ function addcard(num) {
 
 		}
 		cont--;
+		i++;
 	}
 
-	arrayCards = arrayCards.sort(comparador);	
-	arrayCards.forEach( x => divContent.innerHTML += x);
-	
+	arrayCards = arrayCards.sort(comparador);
+	arrayCards.forEach(x => divContent.innerHTML += x);
+
 }
 
+function flipselect(card) {
 
+	let divSelect = document.querySelectorAll(".select");
+
+
+
+	if (!(card.classList.contains("select"))) {
+		card.classList.add("select");
+		card.querySelector(".front-face").style.transform = 'rotateY(-180deg)';
+		card.querySelector(".back-face").style.transform = 'rotateY(0deg)';
+		card.removeAttribute("onclick");
+		divSelect = document.querySelectorAll(".select");
+
+
+		if (divSelect.length == 2) {
+
+			setTimeout(flipbegin, 500);
+		
+		}
+
+	}
+}
+
+function flipbegin() {
+	
+	let select =  document.querySelectorAll(".select");
+	if (select[0].querySelector('.back-face img').src === select[1].querySelector('.back-face img').src) {
+		for (let index = 0; index < select.length; index++) {
+
+			select[index].classList.remove("select");
+			
+		}
+	} else {
+
+		for (let index = 0; index < select.length; index++) {
+
+			select[index].querySelector(".front-face").style.transform = 'rotateY(0deg)';
+			select[index].querySelector(".back-face").style.transform = 'rotateY(180deg)';
+			select[index].classList.remove("select");
+			select[index].setAttribute("onclick", "flipselect(this)");
+
+		}
+	}
+}
 
 entradacartas();
 
@@ -62,7 +106,8 @@ entradacartas();
 
 
 
+//setTimeout()
 
-// querySelector(".front-face").style.transform = 'rotateY(-180deg)';
-// querySelector(".back-face").style.transform = 'rotateY(0deg)';
+// card.querySelector(".front-face").style.transform = 'rotateY(-180deg)';
+// card.querySelector(".back-face").style.transform = 'rotateY(0deg)';
 // teste[0].querySelector('img').src.split('img/')[1]
