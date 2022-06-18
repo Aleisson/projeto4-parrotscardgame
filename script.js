a4e4240c7d47bd3166017f32c401f9889cd55bb2
Aleisson <caioaleisson@gmail.com>
2022-06-18 14:35:52 -0300

    function addcard()

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let img =[]
-
+const img = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
 
 let numCartas = Number(prompt("Quantas cartas quer jogar? Numero entre (4 a 14, sendo par)"));
 
@@ -9,26 +8,59 @@ function comparador() {
 }
 
 
-console.log(typeof (numCartas))
-
-function entradaCartas() {
+function entradacartas() {
 	let cond = 1;
 
 	while (cond) {
 
 		if (!(numCartas % 2) && numCartas >= 4 && numCartas <= 14) {
-			
 			cond = 0;
-
+			addcard(numCartas);
 		} else {
-			
+
 			numCartas = Number(prompt("Quantas cartas quer jogar? Numero entre (4 a 14, sendo par)"));
-	
+
 		}
 	}
 }
 
-entradaCartas();
+function addcard(num) {
+
+	let cont = num / 2;
+	let arrayCards = [];
+	let divContent = document.querySelector(".content");
+
+	while (cont) {
+		for (let index = 0; index < 2; index++) {
+
+			arrayCards.push(`   <div>
+			<div class="card">
+				<div class="front-face face">
+					<img src="img/front.png" alt="papagaio">
+				</div>
+				<div class="back-face face">
+					<img src="img/${img[cont]}" alt="">
+				</div>
+			
+			</div>
+			</div>`)
+
+		}
+		cont--;
+	}
+
+	arrayCards.forEach( x => divContent.innerHTML += x);
+
+
+	
+}
+
+
+
+entradacartas();
+
+
+
 
 
 
